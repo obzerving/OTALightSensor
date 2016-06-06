a0670ef7dcf9534f56ac9aa15c5ef87d2fd15d7e
obzerving <observing@gmail.com>
2016-06-06 16:13:23 -0400

    Initial commit

---
+++ OTALightSensor.ino
@@ -0,0 +1,155 @@
+﻿/* 
+  Light sensor with restful web service.
+  Written for ESP8266 ESP-12, and Adafruit TSL2561 sensor
+
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+
+   I2C Address
+   ===========
+   The address will be different depending on whether you leave
+   the ADDR pin floating (addr 0x39), or tie it to ground or vcc. 
+   The default addess is 0x39, which assumes the ADDR pin is floating
+   (not connected to anything).  If you set the ADDR pin high
+   or low, use TSL2561_ADDR_HIGH (0x49) or TSL2561_ADDR_LOW
+   (0x29) respectively. 
+
+   As of 1/1/2016, the reference to "pgmspace.h" in Adafruit_TSL2561_U.h
+   needs to be changed to <pgmspace.h>
+*/
+// Import required libraries
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <aREST.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <pgmspace.h>
+#include <Adafruit_TSL2561_U.h>
+
+// Configure ADC for supply voltage readings
+ADC_MODE(ADC_VCC);
+// Create aREST instance
+aREST rest = aREST();
+
+const char* ssid = "your_ssid";
+const char* password = "your_password";
+
+// The port to listen for incoming TCP connections 
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int luminance;
+int voltage;
+
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1);
+
+void configureSensor(void)
+{
+  tsl.enableAutoRange(true); /* Auto-gain ... switches automatically between 1x and 16x */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS); /* 16-bit data but slowest conversions */
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("OTA Light Sensor Booting");
+  // Init variables and expose them to REST API
+  luminance = 0;
+  voltage = 0;
+  rest.variable("luminance",&luminance);
+  rest.variable("voltage",&voltage);
+
+  /* Initialise the sensor */
+  if(!tsl.begin())
+  {
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+
+  // Give name and ID to device
+  rest.set_id("1");
+  rest.set_name("front-light-sensor");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("End");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+  // Start the rest server
+  server.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  
+}
+
+void loop() {
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+  delay(1);
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    luminance = event.light;
+//    Serial.print(event.light); Serial.println(" lux");
+  }
+
+  voltage = ESP.getVcc();
+//  Serial.print(voltage); Serial.println(" volts");
+  
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+  delay(1);
+  ArduinoOTA.handle();
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+OTALightSensor
+
+A wifi-enabled Light sensor with restful web service.
+
+Hardware Used:
+- ESP8266-12 wifi module
+- Adafruit TSL2561 sensor
+
+Software Used (Note: Versions may have changed)
+- Arduino development environment for ESP8266 (see https://github.com/esp8266/Arduino)
+- Adafruit TSL2561 Driver v2.0 (Unified Sensor Driver) (see https://learn.adafruit.com/tsl2561/downloads)
+- aREST library, which implements a REST API for Arduino & the ESP8266 WiFi chip (see https://github.com/marcoschwartz/aREST)
+
+Work Flow
+1. Install the development environment
+2. Install the TSL2561 libraries and the aREST library
+3. Change the OTALightSensor.ino file accordingly. I strongly suggest these:
+   a. "your_ssid" to your wifi ssid
+   b. "your_password" to your wifi password
+   c. "front-light-sensor" to a name that makes sense for you sensor location
+4. Wire up the ESP8266 module and the sensor. The components I used require 3.3 volts and do not tolerate 5 volts. It's possible to find modules that can run safely at voltages higher than 3.3 volts, I didn't use them. Be careful.
+5. Program the ESP8266 module. I highly recommend not having the light sensor module connected during programming. My components are mounted in sockets, so I can do that easily. Further, socket mounting these components is a good practice.
+6. Reset the ESP8266 and it should run the program. If you send it a URI of this form:
+
+	http://IP.Address.Of.Sensor/luminance
+
+You should see a JSON response of this form:
+
+	{“luminance”: 00, “id”: “1”, name: “front-light-sensor”, “connected”: true}
+	
+where	"luminance" is the variable name
+		00 will be the actual value (in lux) of the light measured by the sensor
+		"id" corresponds to the ID of the device
+		"1" will be whatever value you gave it in the program (e.g. rest.set_id("1"))
+		name is the name of the device
+		"front-light-sensor" will be whatever name you gave it in the program (e.g. rest.set_name("front-light-sensor"))
+
+It's also possible to find out what the value of the supply voltage is (in case you decide to run it on a battery)
+
+	http://IP.Address.Of.Sensor/voltage
+
+NOTE: At this time, there is a bug in aREST, such that sending the URI with a browser will crash the program. The reason appears to be related to the amount of header information that is sent back by the browser. I have had no problems using libcurl in my programs or curl from the command line (see https://curl.haxx.se/).
+Command line example:
+	curl http://IP.Address.Of.Sensor/luminance
